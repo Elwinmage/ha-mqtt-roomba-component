f11a863b7f0ddde5c56ea90d218c98470cee419d
root <root@a0d7b954-ssh.local.hass.io>
2024-08-12 11:02:30 +0200

    test

--- custom_components/mqttroomba/const.py
+++ custom_components/mqttroomba/const.py
@@ -1,1 +1,22 @@
 """Constants for integration."""
+
+NAME = "MQTT for Roomba"
+DOMAIN = "mqttroomba"
+DOMAIN_DATA = f"{DOMAIN}_data"
+VERSION = "2024.08.12"
+ISSUE_URL = "https://github.com/Elwinmage/ha-mqtt-roomba-component/issues"
+
+PLATFORMS = ["sensor"]
+
+# Defaults
+DEFAULT_NAME = DOMAIN
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+{NAME}
+Version: {VERSION}
+This is a custom integration!
+If you have any issues with this you need to open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
